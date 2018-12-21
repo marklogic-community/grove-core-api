537eeeed57cd9ceab28364daf750b6afb710f454
Geert Josten <geert.josten@marklogic.com>
2018-12-21 12:07:12 +0100

    Applied eslint/prettier formatting rules

--- tests/api/ping.spec.js
+++ tests/api/ping.spec.js
@@ -10,36 +10,30 @@ const groveBaseUrl = 'http://' + groveAppHost + ':' + groveAppPort + '/';
 console.log('Testing ping-api.json against ' + groveBaseUrl);
 
 describe('/api/ping', () => {
-  it('should not listen to something unknown under /api', function () {
-    return frisby
-      .get(groveBaseUrl + 'api/non-existing')
-      .expect('status', 404);
+  it('should not listen to something unknown under /api', function() {
+    return frisby.get(groveBaseUrl + 'api/non-existing').expect('status', 404);
   });
 
-  it('should listen to GET /api/ping', function () {
+  it('should listen to GET /api/ping', function() {
     return frisby
       .get(groveBaseUrl + 'api/ping')
       .expect('status', 200)
       .expect('json', 'ping', 'pong')
       .expect('jsonTypes', '', {
-        'name': Joi.string().optional(),
-        'version': Joi.string().optional()
+        name: Joi.string().optional(),
+        version: Joi.string().optional()
       });
   });
 
-  it('should not listen to non-GET /api/ping', function () {
-    return frisby
-      .post(groveBaseUrl + 'api/ping')
-      .expect('status', 405);
+  it('should not listen to non-GET /api/ping', function() {
+    return frisby.post(groveBaseUrl + 'api/ping').expect('status', 405);
   });
 
-  it('should not listen to anything else below /api/ping', function () {
-    return frisby
-      .get(groveBaseUrl + 'api/ping/pong')
-      .expect('status', 404);
+  it('should not listen to anything else below /api/ping', function() {
+    return frisby.get(groveBaseUrl + 'api/ping/pong').expect('status', 404);
   });
 
-  it('should receive anything', function () {
+  it('should receive anything', function() {
     return frisby
       .fetch(groveBaseUrl + 'api/ping', {
         method: 'GET',
@@ -51,12 +45,12 @@ describe('/api/ping', () => {
       .expect('json', 'ping', 'pong');
   });
 
-  it('should always send json', function () {
+  it('should always send json', function() {
     return frisby
       .fetch(groveBaseUrl + 'api/ping', {
         method: 'GET',
         headers: {
-          'accept': 'application/xml'
+          accept: 'application/xml'
         }
       })
       .expect('status', 200)
